3d1987042675e3564585c689db9ca1eea0830863
Benzeer <Benzeer@LAPTOP-HD4L2JBS>
2023-07-12 15:51:32 +0530

    Add project files.

---
+++ ExtensionDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c = new Calculation();
+            int result = c.Square(2);
+            Console.WriteLine(result);
+
+            int result1 = c.Multiply(4,3);
+            Console.WriteLine(result1);
+
+            
+        }
+    }
+}
